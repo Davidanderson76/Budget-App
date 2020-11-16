bd6a90a2022127380c4eb039d030800394a308c1
David Anderson <david.anderson1993@gmail.com>
2020-11-16 00:35:31 -0600

    db js complete and linked to html

--- public/db.js
+++ public/db.js
@@ -3,13 +3,14 @@
 let db;
 const request = indexedDB.open("budget", 1); //version1
 
+// Create schema
 request.onupgradeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
-    db.event.target.results;
+    db = event.target.result;
 
     if (navigator.onLine) {
         checkDatabase();
@@ -20,3 +21,48 @@ request.onerror = function (event) {
     console.log("~ERROR~" + event.target.errorCode);
 };
 
+
+
+//Opens a transaction,
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if(getAll.result.length > 0) {
+            fetch("/api/transactions/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        };
+    };
+};
+
+
+
+function deletePending() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.clear();
+};
+
+//listener for when and if app comes online//
+window.addEventListener("online", checkDatabase);
