2772dc38626b2a8e059b96bc3a08d25bc44e1d0f
David Anderson <david.anderson1993@gmail.com>
2020-11-16 20:01:34 -0600

    updated db js and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,69 +1,71 @@
-//caching files for offline functionality//
-
 const FILES_TO_CACHE = [
 
-    "/", "/index.html", "index.js", "/style.css", "/db.js", "public/icons/icon-192x192.png"
+    "/", "/index.html", 
+    "index.js", 
+    "/style.css", 
+    "/db.js", 
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
 
 ];
 
-const CACHE_NAME = "static-cache-4";
-const DATA_CACHE_NAME = "data-cache-v4";
+const CACHE_NAME = "static-cache-6";
+const DATA_CACHE_NAME = "data-cache-v6";
 
-self.addEventListener("install", function(evt) {
+self.addEventListener("install", (evt) => {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Files successfully CACHED!");
-            return cache.addAll(FILES_TO_CACHE);
-        })
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
     );
+  
     self.skipWaiting();
-});
-
-
-
-self.addEventListener("activate", function(evt) {
+  });
+  
+  self.addEventListener("activate", (evt) => {
+    // remove old caches
     evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Remove old cache data please!", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
+      caches.keys().then((keyList) => {
+        return Promise.all(
+          keyList.map((key) => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              return caches.delete(key);
+            }
+          })
+        );
+      })
     );
-    self.ClientRectList.claim();
-});
-
-//FETCH
-
-self.addEventListener("fetch", evt => {
-    if(evt.request.url.includes("/api/")) {
-        console.log("[Service Worker] Fetch(data)", evt.request.url);
-
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request).then(response => {
-                    if(response.status === 200){
-                        cache.put(evt.request.url, response.clone());
-                    }
-                    return response;
-                })
-                .catch(err => {
-                    return cache.match(evt.request);
-                });
-            })
-        );
-        return;
+  
+    self.clients.claim();
+  });
+  
+  self.addEventListener("fetch", (evt) => {
+    if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
+      evt.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then((cache) => {
+            return fetch(evt.request)
+              .then((response) => {
+                if (response.status === 200) {
+                  cache.put(evt.request, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(() => {
+                return cache.match(evt.request);
+              });
+          })
+          .catch((err) => console.log(err))
+      );
+      return;
     }
-
-        evt.respondWith(
-            caches.open(CACHE_NAME).then(cache => {
-                return cache.match(evt.request).then(response => {
-                    return response || fetch(evt.request);
-                });
-            })
-        );
-});
+  
+    evt.respondWith(
+      caches.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      })
+    );
+  });
+  
--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
-mongoose.connect("mongodb://localhost/budget", {
+const mongoUri = process.env.MONGODB_URI || "mongodb://localhost/budget";
+mongoose.connect(mongoUri, {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
